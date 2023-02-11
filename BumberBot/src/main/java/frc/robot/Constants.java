848b5df9e8ed720800a79aedfa2663e43d62d8d5
SherazF <Sheraz24132@gmail.com>
2023-02-11 11:44:45 -0800

    Driving Works

--- BumberBot/src/main/java/frc/robot/Constants.java
+++ BumberBot/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +17,40 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public final class USB {
+    public static final int leftJoystick = 0;
+    public static final int rightJoystick = 1;
+    public static final int xBoxController = 2;
+  }
+
+  public static final class Pneumatics {
+    public static final int pcmOne = 11;
+    public static final PneumaticsModuleType pcmType =
+        PneumaticsModuleType.CTREPCM; // CTREPCM, REVPH
+
+    public static final int intakePistonForward = pcmType == PneumaticsModuleType.CTREPCM ? 0 : 0;
+    public static final int intakePistonReverse = pcmType == PneumaticsModuleType.CTREPCM ? 1 : 1;
+  }
+
+  public static class DriveTrainConstants { 
+    public static final int pigeonID = 9; 
+    public static final int FrontLeftDriveMotor = 21; 
+    public static final int FrontRightDriveMotor = 22; 
+    public static final int BackLeftDriveMotor = 23; 
+    public static final int BackRightDriveMotor = 24; 
+    
+    public enum DriveTrainNeutralMode {
+      BRAKE,
+      COAST,
+      /** Master motors brake, follower motors coast */
+      HALF_BRAKE
+    }
+  }
+  
+  public static class IntakeConstants { 
+    public static final int intakeMotor = 31; 
+    public static final int encoderUnitsPerRotation = 2048;
   }
 }
